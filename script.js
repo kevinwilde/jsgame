0cade504f6acb8c4339c7d59d4ef88bc5c81df34
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2015-11-02 17:45:17 -0600

    Basic game

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-$('#test').hover(function () {
-    $(this).css('color', 'red');
+var livesLeft = 3;
+var score = 0
+var speed = 5000;
+
+$('.btn-play-game').click(function () {
+    $('.dv-play-game').fadeOut(300, "linear", createRock);
 });
+
+function createRock() {
+    speed = Math.floor(speed * 0.9);
+    var rock = document.createElement("div");
+    $(rock).addClass('rock');
+    $(rock).css('left', Math.floor(($(window).width() - 110) * Math.random()));
+    $(rock).css('bottom', $(window).height() - 5);
+    document.body.appendChild(rock);
+    $(rock).animate({ bottom: 0 }, speed, "linear", loseLife);
+    $(rock).click(function () {
+        $(rock).stop();
+        $(rock).remove();
+        score += 1;
+        $('#score').text(score);
+        createRock();
+    });
+}
+
+function loseLife() {
+    livesLeft -= 1;
+    $('#lives-left').text(livesLeft);
+    if (livesLeft <= 0) {
+        gameOver();
+    }
+    else {
+        createRock();
+    }
+}
+
+function gameOver() {
+    $('.rock').remove();
+    var btnReplay = document.createElement("a");
+    $(btnReplay).addClass('btn-play-game');
+    $(btnReplay).text("Play Again");
+    document.body.appendChild(btnReplay);
+    //$(btnReplay).click(createRock);
+}
