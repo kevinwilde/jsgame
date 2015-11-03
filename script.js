9ff991629342cfe9dc620046a3e67b960ac3367d
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2015-11-03 15:55:54 -0600

    Create rock at decreasing intervals rather than when a rock is destroyed

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 var livesLeft = 3;
 var score = 0
 var speed = 5000;
+var interval = null;
 
 $('#btn-start').click(function () {
-    $('#dv-start').fadeOut(300, "linear", createRock);
+    $('#dv-start').fadeOut(300);
+    createRock();
 });
 
 function createRock() {
-    speed = Math.floor(speed * 0.9);
     var rock = document.createElement("div");
     $(rock).addClass('rock');
     $(rock).css('left', Math.floor(($(window).width() - 110) * Math.random()));
     $(rock).css('bottom', $(window).height() - 5);
     document.body.appendChild(rock);
-    $(rock).animate({ bottom: 0 }, speed, "linear", loseLife);
+    clearInterval(interval);
+    speed = Math.floor(speed * 0.9); 
+    if (speed < 450) { // never let speed go below 450
+        speed = 450;
+    }
+    interval = setInterval(createRock, speed);
+    $(rock).animate({ bottom: 0 }, 5000, "linear", loseLife);
     $(rock).click(function () {
         if (!($(rock).hasClass('bottom'))) {
             $(rock).stop();
             $(rock).remove();
             score += 1;
             $('.score').text(score);
-            createRock();
         }
     });
 }
@@ -32,14 +38,12 @@ function loseLife() {
     if (livesLeft <= 0) {
         gameOver();
     }
-    else {
-        createRock();
-    }
 }
 
 function gameOver() {
+    clearInterval(interval);
+    $('.rock').addClass('bottom');
     $('.rock').stop();
-    //$('.dv-score').fadeOut(300);
     $('#dv-replay').fadeIn(300);
 }
 
@@ -51,5 +55,4 @@ $('#btn-replay').click(function () {
     $('.score').text(score);
     $('.lives-left').text(livesLeft);
     $('#dv-replay').fadeOut(300, "linear", createRock);
-    //$('.dv-score').fadeIn(300);
 });
